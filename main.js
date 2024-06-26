4dd278faa6261d2f6f9e7f6c7452af2e81d97456
Soulseb8 <csbastian88@gmail.com>
2024-06-26 14:31:08 -0300

    Se hicieron cambios en el home y se modificaron los colores de los elementos

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
 var settingsmenu = document.querySelector(".settings-menu");
+var iconousuario = document.getElementById("iconousuario");
 var darkBtn = document.getElementById ("dark-btn");
 
 function settingsMenuToggle(){
     settingsmenu.classList.toggle("settings-menu-height");
 }
+iconousuario.addEventListener("click",settingsMenuToggle());
+
+
 darkBtn.onclick = function(){
     darkBtn.classList.toggle("dark-btn-on");
     document.body.classList.toggle("dark-theme");

eb8936d85e922c967f22ca4964025cb0a1abe7ff
Soulseb8 <csbastian88@gmail.com>
2024-06-26 21:39:06 -0300

    Modificamos el archivo JS y sacamos lineas para poder hacer la ventana de settings funcional

--- main.js
+++ main.js
@@ -1,36 +1,7 @@
 var settingsmenu = document.querySelector(".settings-menu");
-var iconousuario = document.getElementById("iconousuario");
-var darkBtn = document.getElementById ("dark-btn");
+
 
 function settingsMenuToggle(){
     settingsmenu.classList.toggle("settings-menu-height");
 }
-iconousuario.addEventListener("click",settingsMenuToggle());
-
-
-darkBtn.onclick = function(){
-    darkBtn.classList.toggle("dark-btn-on");
-    document.body.classList.toggle("dark-theme");
-
-    if(localStorage.getItem("theme") == "light"){
-        localStorage.setItem("theme", "dark");
-    }
-    else{
-        localStorage.setItem("theme", "light");
-    }
-
-}
-
-if(localStorage.getItem("theme") == "light"){
-    darkBtn.classList.remove("dark-btn-on");
-    document.body.classList.remove("dark-theme");
-}
-else if(localStorage.getItem("theme") == "dark"){
-    darkBtn.classList.add("dark-btn-on");
-    document.body.classList.add("dark-theme");
-}
-else{
-    localStorage.setItem("theme", "light");
-}
-
 
